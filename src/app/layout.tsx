b3c76f9bc572940f297f0283da1a27da00f6952f
Danilo Barros <danilobjr@gmail>
2023-01-18 03:37:20 -0300

    chore: setup nunito font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
+import { Nunito } from '@next/font/google'
 import { ReactNode } from 'react'
 import './globals.css'
+
+const nunitoFont = Nunito({
+  style: ['normal'],
+  weight: ['500'],
+  subsets: ['latin'],
+  variable: '--font-nunito',
+})
 
 type RootLayoutProps = {
   children: ReactNode
@@ -7,7 +15,7 @@ type RootLayoutProps = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="en" className={nunitoFont.variable}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
