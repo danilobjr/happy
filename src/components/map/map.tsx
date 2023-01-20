518f851c289a7ef2883c56bcdbaa0ef23eafb1ef
Danilo Barros <danilobjr@gmail>
2023-01-20 11:49:37 -0300

    feat(map): set icon anchor

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -5,11 +5,9 @@ import 'leaflet/dist/leaflet.css'
 
 export function Map() {
   // TODO memoize this
-  // FIXME set anchor position
-  const markerIcon = icon({ iconUrl: '/marker.png' })
+  const markerIcon = icon({ iconUrl: '/marker.png', iconAnchor: [24, 54] })
 
   return (
-    // TODO use mapbox instead?
     <MapContainer
       className="absolute h-full w-full"
       center={[51.505, -0.09]}
@@ -18,12 +16,12 @@ export function Map() {
     >
       <TileLayer
         url={env.mapboxTileServerUrl}
-        // attribution={
-        //   '&copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>'
-        // }
+        attribution={
+          '&copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a>, &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>'
+        }
       />
       <Marker position={[51.505, -0.09]} icon={markerIcon}>
-        <Popup>This is a popup</Popup>
+        {/* <Popup>This is a popup</Popup> */}
       </Marker>
     </MapContainer>
   )
